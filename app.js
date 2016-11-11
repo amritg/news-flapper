1ec063f62302f3ef30de9c1a962383af5a4f5de1
Amrit Gautam <gautamamrit24@gmail.com>
2016-11-11 23:27:48 +0200

    Add styling to the page

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 (function (){
-    var app = angular.module('flapperNews',[]);
-    app.controller('MainCtrl',['$scope',function($scope){
+    var app = angular.module('flapperNews',['ui.router']);
+
+    app.controller('MainCtrl',['$scope','posts',function($scope,posts){
         $scope.test = "Hello world!";
         $scope.title = "";
         $scope.link = "";
-        $scope.posts = [
-            {title:'post 1', link:'link1', upvotes: 5},
-            {title:'post 2', link:'link2', upvotes: 2},
-            {title:'post 3', link:'link3', upvotes: 15},
-            {title:'post 4', link:'link4', upvotes: 9},
-            {title:'post 5', link:'link5', upvotes: 4}
-        ];
+        $scope.posts = posts.posts;
         $scope.addPost = function(){
             if(!$scope.title || $scope.title === ''){
                 return;
@@ -24,4 +19,31 @@
             post.upvotes += 1;
         };
     }]);
+
+    app.factory('posts',[function(){
+        var  o = {
+            posts: [
+                {title:'post 1', link:'link1', upvotes: 5},
+                {title:'post 2', link:'link2', upvotes: 2},
+                {title:'post 3', link:'link3', upvotes: 15},
+                {title:'post 4', link:'link4', upvotes: 9},
+                {title:'post 5', link:'link5', upvotes: 4}
+            ]
+        };
+        return o;
+    }]);
+
+    app.config([
+        '$stateProvider',
+        '$urlRouterProvider',
+        function($stateProvider,$urlRouterProvider){
+            $stateProvider
+                .state('home',{
+                    url: '/home',
+                    templateUrl: '/home.html',
+                    controller: 'MainCtrl'
+                });
+            $urlRouterProvider.otherwise('home');
+        }
+    ]);
 })();

9424a14fb9e1e30a3f8934ffb84c80314d3fec49
Amrit Gautam <gautamamrit24@gmail.com>
2016-11-16 15:54:48 +0200

    Finish Backend support

--- app.js
+++ app.js
@@ -8,8 +8,24 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
 
+var app = express();
+// **
+var mongoose = require('mongoose');
+
+require('./models/Comment');
+require('./models/Post');
+
+mongoose.connect('mongodb://localhost:27017/news');
+
+var db = mongoose.connection;
+db.on('error', function() {
+    console.log('connection error');
+});
+db.once('open', function() {
+    console.log("Connection successful !");
+});
+// **
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -27,20 +43,20 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,72 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Post = require('../models/Post');
+var Comment = require('../models/Comment');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/posts', function(req,res,next){
+  Post.find({},function(err,posts){
+      if(err) {return next(err)};
+      res.json(posts);
+  });
+});
+
+router.post('/posts', function(req,res,next){
+   var post = new Post(req.body);
+
+   post.save(function(err,post){
+     if(err) {return next(err);}
+     res.json(post);
+   });
+});
+
+router.get('/posts/:post',function(req,res,next){
+  Post.findById(req.params.post)
+    .populate('comments')
+    .exec(function(err, post){
+        if(err) throw err;
+        res.json(post);
+    });
+});
+
+router.put('/posts/:post/upvote',function(req, res, next) {
+  Post.findById(req.params.post)
+    .exec(function(err, post){
+      post.upvote(function(err, post){
+        if (err) { return next(err); }
+        res.json(post);
+      });
+    });
+});
+
+router.post('/posts/:post/comments',function(req, res, next){
+    Post.findById(req.params.post)
+        .exec(function(err, post){
+    
+            var comment = new Comment(req.body);
+    
+            comment.post = post;
+
+            comment.save(function(err, comment){
+                if(err){ return next(err);}
+                post.comments.push(comment);
+
+                post.save(function(err,post){
+                    if(err) {return next(err);}
+                    res.json(post);
+                });
+            });
+        });
+});
+
+router.put('/posts/:post/comments/:comment/upvote',function(req, res, next){
+   //fill here
+});
 module.exports = router;

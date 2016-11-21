d4cefb584e7ed69e8409e06158af287c0ebe8db9
Amrit Gautam <gautamamrit24@gmail.com>
2016-11-21 16:30:03 +0200

    Ready app to deploy to Heroku

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    'mongoUrl' : 'mongodb://localhost:27017/news'
+    // 'mongoUrl' : 'mongodb://localhost:27017/news'
+    'mongoUrl' : 'mongodb://amritg:hetauda24@ds159507.mlab.com:59507/news'
 }
